76794a9e14a8070d4805fd0efbbf7f55ecfc7ddb
Simon Farås <unilazor@Simons-MacBook-Pro-4.local>
2017-05-19 10:51:17 +0200

    simulation lite mer generisk

--- static/simulation.js
+++ static/simulation.js
@@ -1,132 +1,151 @@
-function main() {
-    let i = 0;
-    let completeMember;
-    let isComplete = false;
-    const population = new Population({
-        goal: 'Hello, World!',
-        size: 1000
+"use strict";
+(function(global) {
+    // const { GenLab: { World }} = global;
+
+    const globalSettings = {
+        populationSize: 100,
+        maxGeneration: 1000,
+        mutationChance: 0.1,
+        topFitnessShare: 0.3,
+    }
+
+    // Population sköter generationer med hjälp av urval, crossover och muteringar
+    function population(fitness, crossover, mutate, mutationChance = globalSettings.mutationChance) {
+        let generationNumber = 0;
+
+        const sort = (chromosomes) => {
+            const sortedChromosomes = chromosomes.slice(0);
+            sortedChromosomes.sort((a, b) => a.fitness - b.fitness);
+
+            return sortedChromosomes;
+        }
+
+        const calcFitness = (chromosomes) => {
+            return chromosomes.map(chromosome => ({
+                chromosome,
+                fitness: fitness(chromosome)
+            }));
+        }
+
+        // Kör denna efter en simulation och 
+        const getNewGeneration = (currentGeneration) => {
+            // Lägg till fitness på alla kromosomer
+            const rankedGeneration = calcFitness(currentGeneration);
+            // Sortera på fitness och ta ut själva kromosomen
+            const sortedGeneration = sort(rankedGeneration).map(item => item.chromosome);
+
+            let crossoverCandidateCount = Math.floor(sortedGeneration.length * globalSettings.topFitnessShare);
+
+            const crossoverCandidates = sortedGeneration.slice(0, crossoverCandidateCount);
+            const crossovers = crossoverCandidates.map((chromosome, i, arr) => {
+                // Gör crossover mot sista elementet om detta är det första
+                const second = i !== 0 ? arr[i - 1] : arr[arr.length - 1];
+                
+                return crossover(chromosome, second);
+            })
+
+            generationNumber++;
+
+            const newGeneration = [
+                ...crossoverCandidates,
+                ...crossovers,
+                // Fyll ut till förra generationens storlek genom att ta med de som blir över
+                ...sortedGeneration.slice(crossoverCandidateCount, currentGeneration.length - crossovers.length)
+            ];
+
+            // Return new generation with mutations
+            return newGeneration.map(chromosome => {
+                if (Math.random() <= globalSettings.mutationChance) {
+                    return mutate(chromosome);
+                }
+
+                return chromosome;
+            })
+        }
+
+        return {
+            getGenerationNumber: () => generationNumber,
+            getNewGeneration
+        }
+    }
+
+    // Denna beror på vilken typ av grejjer vi har
+    const simulationSettings = goal => ({
+        fitness: function (word) {
+            let total = 0;
+            for(let i = 0; i < word.length; i++) {
+                const score = word.charCodeAt(i) - goal.charCodeAt(i);
+                total += score * score;
+            }
+
+            return total;
+        },
+        crossover: (first, second) => {
+            const pivot = Math.round(first.length / 2) - 1;
+
+            return first.substr(0, pivot) + second.substr(pivot);
+        },
+        mutate: word => {
+            const index = Math.floor(Math.random() * word.length);
+            const change = Math.random() > 0.5 ? 1 : -1;
+            const codeAsCharCodes = [...word].map(char => char.charCodeAt(0));
+            let mutatedCharCode = codeAsCharCodes[index] + change;
+
+            if (mutatedCharCode < 0) {
+                mutatedCharCode = 255;
+            }
+
+            if (mutatedCharCode > 255) {
+                mutatedCharCode = 0;
+            }
+
+            return String.fromCharCode(...[
+                ...codeAsCharCodes.slice(0, index),
+                mutatedCharCode,
+                ...codeAsCharCodes.slice(index + 1)
+            ]);
+        },
+        isCompleted: generation => {
+            return generation[0] === goal;
+        },
+        createChromosome: () => {
+            let word = ''
+
+            for (let i = 0; i < goal.length; i++) {
+                word += String.fromCharCode(Math.floor(Math.random() * 255));
+            }
+
+            return word;
+        }
     });
 
-    const display = function(pop) {
-        console.log('Generation', pop.generationNumber, "Best individuals", pop.members[0].code, pop.members[1].code);
-    }  
-
-    for (let i = 0; i < 1000000; i++) {
-        population.generation();
-        
-        completeMember = population.getCompleteMember();
-        if (completeMember) {
-            break;
+    function runSimulation(settings) {
+        let currentGeneration = [];
+        for (let i = 0; i < globalSettings.populationSize; i++) {
+            currentGeneration.push(settings.createChromosome(settings));
         }
 
-        display(population);
+        const pop = population(
+            settings.fitness,
+            settings.crossover,
+            settings.mutate
+        )
+
+        for (let i = 0; i < globalSettings.maxGeneration; i++) {
+            currentGeneration = pop.getNewGeneration(currentGeneration);
+
+            if (settings.isCompleted(currentGeneration)) {
+                break;
+            }
+        }
+
+        displayResult(pop, currentGeneration);
     }
 
-    console.log('Done', completeMember.code);
-}
-
-function Chromosome(code) {
-    if (code) {
-        this.code = code;
+    function displayResult(pop, generation) {
+        console.log(`Simulation ended at generation ${pop.getGenerationNumber()} with best chromosome ${generation[0]}`);
     }
 
-    this.cost = 9999;
-}
+    runSimulation(simulationSettings("bajs"));
 
-Chromosome.prototype = Object.assign({}, {
-    code: '',
-    random(length) {
-        while(length--) {
-            this.code += String.fromCharCode(Math.floor(Math.random()*255));
-        }
-    },
-    calcCost(compareTo) {
-        let total = 0;
-        for(let i = 0; i < this.code.length; i++) {
-            const score = this.code.charCodeAt(i) - compareTo.charCodeAt(i);
-            total += score * score; // Square score to avoid
-        }
-
-        this.cost = total;
-    },
-    mutate(chance) {
-        if (Math.random() > chance) {
-            return;
-        }
-
-        const index = Math.floor(Math.random() * this.code.length);
-        const change = Math.random() > 0.5 ? 1 : -1;
-        const codeAsCharCodes = [...this.code].map(char => char.charCodeAt(0));
-        let mutatedCharCode = codeAsCharCodes[index] + change;
-
-        if (mutatedCharCode < 0) {
-            mutatedCharCode = 255;
-        }
-
-        if (mutatedCharCode > 255) {
-            mutatedCharCode = 0;
-        }
-
-        this.code = String.fromCharCode(...[
-            ...codeAsCharCodes.slice(0, index),
-            mutatedCharCode,
-            ...codeAsCharCodes.slice(index + 1)
-        ]);
-    },
-    crossover(chromosome) {
-        const pivot = Math.round(this.code.length / 2) - 1;
-
-        const child1Code = this.code.substr(0, pivot) + chromosome.code.substr(pivot);
-        const child2Code = chromosome.code.substr(0, pivot) + this.code.substr(pivot);
-
-        return [
-            new Chromosome(child1Code),
-            new Chromosome(child2Code)
-        ];
-    }
-});
-
-function Population({goal, size, mutationChance = 0.3}) {
-    this.members = [];
-    this.goal = goal;
-    this.generationNumber = 0;
-    this.mutationChance = mutationChance;
-
-    while(size--) {
-        const chromsome = new Chromosome();
-        chromsome.random(this.goal.length);
-        this.members.push(chromsome);
-    }
-}
-
-Population.prototype = Object.assign({}, {
-    sort() {
-        this.members.sort((a, b) => a.cost - b.cost);
-    },
-    generation() {
-        // Räkna ut hur "bra" varje individ är
-        for (let i = 0; i < this.members.length; i++) {
-            this.members[i].calcCost(this.goal);
-        }
-
-        this.sort();
-
-        // Kombinera de två bästa
-        const [child1, child2] = this.members[0].crossover(this.members[1]);
-
-        // Ersätt de två sämsta individerna med barnen av de två bästa
-        this.members.splice(this.members.length - 2, 2, child1, child2);
-
-        // Mutera individer
-        for (let i = 0; i < this.members.length; i++) {
-            this.members[i].mutate(this.mutationChance);
-        }
-
-        this.generationNumber++;
-    },
-    getCompleteMember() {
-        return this.members.find(member => member.code === this.goal);
-    }
-})
-
-main();
+})(window);
